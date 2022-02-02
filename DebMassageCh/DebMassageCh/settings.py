e0ead60d48a20cbfc166b34f19e7a84c2649c74b
ycrausaz <yann.crausaz@gmail.com>
2022-02-02 23:02:09 +0100

    Prepared again for Heroku

--- DebMassageCh/DebMassageCh/settings.py
+++ DebMassageCh/DebMassageCh/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +28,8 @@ SECRET_KEY = 'django-insecure-@c$vb1uc=^lz@5*n@#u*g0a+xrwfseq09-2^0nkatsvy!8=3at
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['DebMassageCh.herokuapp.com']
 
 
 # Application definition
@@ -135,7 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
